35bd94dd7a214024bb46830656c440660abc24ef
Kara Bayse <karabayse@gmail.com>
2019-02-08 18:04:26 -0600

    added constructor

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ console.log('in index.js');
 
 const person = require('./person');
 console.log(person);
+
+const Person2 = require('./person');
+const newPerson = new Person2('June Doe', 50);
+newPerson.greeting();
--- person.js
+++ person.js
@@ -3,4 +3,15 @@ const person = {
   age: '45'
 }
 
+class Person2 {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+  greeting() {
+    console.log(`My name is ${this.name} and I am ${this.age} years old`);
+  }
+}
+
 module.exports = person;
+module.exports = Person2;
