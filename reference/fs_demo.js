d8acb5cc6ea2a8e9922bd8deef671eefa07e268f
Kara Bayse <karabayse@gmail.com>
2019-02-15 19:52:38 -0600

    create and write to file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -7,3 +7,10 @@ fs.mkdir(path.join(__dirname, '/test'), {}, (err) => {
   if(err) throw err;
   console.log('Folder created');
 });
+
+// Create and write to file
+fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', (err) => {
+  // Check for error
+  if(err) throw err;
+  console.log('File written to...');
+});
